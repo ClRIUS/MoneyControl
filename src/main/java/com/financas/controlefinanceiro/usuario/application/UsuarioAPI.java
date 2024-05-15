1eac1391fff4ad8f29fd0f506f66c520897745db
= <gabriel.cirilo.rosa@gmail.com>
2024-05-15 20:33:16 -0300

    [cadastraUsuario] Implementa Camada API e fluxo para Controller

--- src/main/java/com/financas/controlefinanceiro/usuario/application/UsuarioAPI.java
+++ src/main/java/com/financas/controlefinanceiro/usuario/application/UsuarioAPI.java
@@ -1,2 +1,13 @@
-package com.financas.controlefinanceiro.usuario.application;public interface UsuarioAPI {
+package com.financas.controlefinanceiro.usuario.application;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/usuario")
+public interface UsuarioAPI {
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    UsuarioResponse cadastraUsuario(@Valid @RequestBody UsuarioRequest usuarioRequest);
 }
--- src/main/java/com/financas/controlefinanceiro/usuario/application/UsuarioController.java
+++ src/main/java/com/financas/controlefinanceiro/usuario/application/UsuarioController.java
@@ -1,2 +1,15 @@
-package com.financas.controlefinanceiro.usuario.application;public class UsuarioController {
+package com.financas.controlefinanceiro.usuario.application;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class UsuarioController implements UsuarioAPI{
+    @Override
+    public UsuarioResponse cadastraUsuario(UsuarioRequest usuarioRequest) {
+        log.info("[Start] UsuaioController - CadastraUsuario");
+        log.info("[Finish] UsuaioController - CadastraUsuario");
+        return null;
+    }
 }
